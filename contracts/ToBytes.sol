21bc7b1bf4a56b2002ebf63d9d132b8910bbdf59
Alexander <alexanderattar@gmail.com>
2022-12-20 15:21:45 -0500

    Add ToBytes.sol

---
+++ contracts/ToBytes.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Storage {
+    function toBytes(
+        address from,
+        address to,
+        uint256 amount
+    ) public pure returns (bytes memory) {
+        return abi.encode(from, to, amount);
+    }
+}
