a5a4e69e2167abcc2e2a03f00f38f9682fc7d95c
Alexander <alexanderattar@gmail.com>
2023-01-15 14:42:22 -0500

    Add DOS mitigation to king of ether contract

--- contracts/KingOfEther.sol
+++ contracts/KingOfEther.sol
@@ -56,3 +56,33 @@ contract Attack {
         kingOfEther.claimThrone{value: msg.value}();
     }
 }
+
+// MITIGATION
+// SPDX-License-Identifier: MIT
+// One way to prevent this is to allow the users to withdraw their Ether instead of sending it.
+pragma solidity ^0.8.17;
+
+contract KingOfEther {
+    address public king;
+    uint256 public balance;
+    mapping(address => uint256) public balances;
+
+    function claimThrone() external payable {
+        require(msg.value > balance, "Need to pay more to become the king");
+
+        balances[king] += balance;
+
+        balance = msg.value;
+        king = msg.sender;
+    }
+
+    function withdraw() public {
+        require(msg.sender != king, "Current king cannot withdraw");
+
+        uint256 amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+
+        (bool sent, ) = msg.sender.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+}
